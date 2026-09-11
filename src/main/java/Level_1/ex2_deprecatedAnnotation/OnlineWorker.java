6829bc3f8c5124df40df9951b83c70912b0f221c
Egliz <egliz3010@gmail.com>
2026-09-11 11:57:13 +0200

    feat: add OnlineWorker class

---
+++ src/main/java/Level_1/ex2_deprecatedAnnotation/OnlineWorker.java
@@ -0,0 +1,19 @@
+package Level_1.ex2_deprecatedAnnotation;
+
+public class OnlineWorker extends Worker{
+    private static final double INTERNET_PLAN = 8.7;
+
+    public OnlineWorker(String name, String surname, double hourlyRate){
+        super(name, surname, hourlyRate);
+    }
+
+    @Override
+    public double calculateSalary(int hoursWorked) {
+        return hoursWorked * hourlyRate + INTERNET_PLAN;
+    }
+
+    @Override
+    public String toString() {
+        return "OnSite" + super.toString() ;
+    }
+}
