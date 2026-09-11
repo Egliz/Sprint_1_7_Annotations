46c7fa2cea98bfb5f2c2f792282d74b3a5141b1c
Egliz <egliz3010@gmail.com>
2026-09-11 19:15:13 +0200

    feat: add custom annotation

---
+++ src/main/java/Level_2/ex1_jsonSerialization/CustomAnnotationForJson.java
@@ -0,0 +1,13 @@
+package Level_2.ex1_jsonSerialization;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+
+public @interface CustomAnnotationForJson {
+    String directory();
+}
