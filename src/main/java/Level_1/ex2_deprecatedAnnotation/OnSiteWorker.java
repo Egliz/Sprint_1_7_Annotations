ce865a2c088a8b9212c0b7edc0116b7f9dfe8a26
Egliz <egliz3010@gmail.com>
2026-09-11 11:56:20 +0200

    feat: add OnSiteWorker class

---
+++ src/main/java/Level_1/ex2_deprecatedAnnotation/OnSiteWorker.java
@@ -0,0 +1,19 @@
+package Level_1.ex2_deprecatedAnnotation;
+
+public class OnSiteWorker extends Worker{
+    private static double gas = 6.4;
+
+    public OnSiteWorker(String name, String surname, double hourlyRate) {
+        super(name, surname, hourlyRate);
+    }
+
+    @Override
+    public double calculateSalary(int hoursWorked) {
+        return hoursWorked * hourlyRate + gas;
+    }
+
+    @Override
+    public String toString() {
+        return "OnSite " + super.toString();
+    }
+}
