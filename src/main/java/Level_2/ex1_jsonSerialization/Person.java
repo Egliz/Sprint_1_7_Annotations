a9c9aa5f6a3138c5f9b096ab147a3f23d49f94cd
Egliz <egliz3010@gmail.com>
2026-09-11 18:52:25 +0200

    feat: add Person class

---
+++ src/main/java/Level_2/ex1_jsonSerialization/Person.java
@@ -0,0 +1,23 @@
+package Level_2.ex1_jsonSerialization;
+
+public class Person {
+
+    private String name;
+    private String surname;
+    private int age;
+
+    public Person(String name, String surname, int age){
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
