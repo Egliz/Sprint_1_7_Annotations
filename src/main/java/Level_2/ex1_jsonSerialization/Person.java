3c1f3d36e74a87eeabe5f17783966a38e9ab56d0
Egliz <egliz3010@gmail.com>
2026-09-12 12:45:11 +0200

    feat: add getters in Person class

--- src/main/java/Level_2/ex1_jsonSerialization/Person.java
+++ src/main/java/Level_2/ex1_jsonSerialization/Person.java
@@ -14,6 +14,18 @@ public class Person {
         this.age = age;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
     @Override
     public String toString() {
         return "Person{" +
