d1d54eaed9bb60a533357e15e38497e8308588fe
Egliz <egliz3010@gmail.com>
2026-09-13 12:09:57 +0200

    feat: implement serialize method

--- src/main/java/Level_2/ex1_jsonSerialization/JsonControl.java
+++ src/main/java/Level_2/ex1_jsonSerialization/JsonControl.java
@@ -18,4 +18,23 @@ public class JsonControl {
             return null;
         }
     }
+
+    public void serialize(File directory, Person person) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            String jsonString = objectMapper.writeValueAsString(person);
+            System.out.println(jsonString);
+            String filePath = directory + File.separator + "SerializedObject.json";
+
+            try (FileWriter fileWriter = new FileWriter(filePath);
+                 ){
+                fileWriter.write(jsonString);
+
+            } catch (IOException e){
+                System.out.println("Error writing to file." + e.getMessage());
+            }
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+}
 }
--- src/main/java/Level_2/ex1_jsonSerialization/Main.java
+++ src/main/java/Level_2/ex1_jsonSerialization/Main.java
@@ -1,26 +1,28 @@
 package Level_2.ex1_jsonSerialization;
 
 
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
 
         Person person = new Person("Lady", "Gaga", 30);
         JsonControl jsonControl = new JsonControl();
 
-        String directory = jsonControl.getDirectoryFromAnnotation(person);
+        String directoryFromAnnotation = jsonControl.getDirectoryFromAnnotation(person);
+        File directory = new File(directoryFromAnnotation);
 
         if(checkDirectory(directory)) {
-           // serializar desde jsonControl.serialize(directory, person);
+           jsonControl.serialize(directory, person);
         } else {
             throw new IllegalArgumentException("Directory no valid.");
         }
     }
 
-    public static boolean checkDirectory(String directory) {
-        if(directory == null || directory.isBlank()) {
-            return false;
+    public static boolean checkDirectory(File directory) {
+        if(directory.exists() && directory.isDirectory()) {
+            return true;
         }
-        return true;
+        return false;
     }
 }
-
